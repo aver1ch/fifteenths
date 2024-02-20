6c97f106608bc24433a22fe364a26aa4c1e80ea4
aver1ch <vavericherry3@gmail.com>
2024-02-21 00:15:43 +0300

    Deleted comment

--- src/Fifteenths.java
+++ src/Fifteenths.java
@@ -40,7 +40,6 @@ public class Fifteenths
             JButton plate = new JButton(" " + i + " ");
             plate.setPreferredSize(new Dimension(100, 100));
             buttons.add(plate);
-            //lol
         }
     }
 
