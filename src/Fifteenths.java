854df4280ad80ed73a61bd373f715ab2777898c0
aver1ch <vavericherry3@gmail.com>
2024-02-21 20:45:19 +0300

    Added shuffle

--- src/Fifteenths.java
+++ src/Fifteenths.java
@@ -1,18 +1,19 @@
 import javax.swing.*;
 
 import java.awt.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 // первая, которая считает, сколько ячеек находится не на своём месте
 // вторая, которая считает количество ходов до правильной комбинации
-// сделать игру и сделать, чтобы играла сама игра)
 // эвристика
 // uml-модели
 
-public class Fifteenths
+final public class Fifteenths
 {
     private final JFrame frame = new JFrame("Пятнашки");
+    private List<JButton> buttons = new ArrayList<>();
 
     final public void window()
     {
@@ -20,7 +21,7 @@ public class Fifteenths
         frame.setLayout(new FlowLayout());
         frame.setSize(700, 700);
         frame.setVisible(true);
-
+        
         createPlaystation();
         createScoreTable();
     }
@@ -29,21 +30,24 @@ public class Fifteenths
     {
         JPanel playStation = new JPanel(new GridLayout(4, 4, 5, 5));
         playStation.setBorder(BorderFactory.createTitledBorder("Game"));
-        Map<Integer, JButton> buttons = new HashMap<>();
-        createButtons(playStation, buttons);
+        createButtons(playStation);
         frame.getContentPane().add(playStation);
         frame.pack();
         frame.setVisible(true);
     }
 
-    public void createButtons(JPanel playStation, Map<Integer, JButton> buttons)
+    public void createButtons(JPanel playStation)
     {
         for (int i = 1; i <= 15; ++i)
         {
             JButton plate = new JButton(" " + i + " ");
             plate.setPreferredSize(new Dimension(100, 100));
-            buttons.put(i, plate);
-            playStation.add(buttons.get(i));
+            buttons.add(i - 1, plate);
+        }
+        shuffle();
+        for (int i = 1; i <= 15; ++i)
+        {
+            playStation.add(buttons.get(i - 1));
         }
     }
 
@@ -63,4 +67,19 @@ public class Fifteenths
         frame.pack();
         frame.setVisible(true);
     }
+
+    public int firstHeuristicProblem()
+    {
+        return 0;
+    }
+
+    public int secondHeuristicProblem()
+    {
+        return 0;
+    }
+
+    public void shuffle()
+    {
+        Collections.shuffle(buttons);
+    }    
 }
